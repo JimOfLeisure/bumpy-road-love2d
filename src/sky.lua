05db2a8948f94988f2f42e88433d7e107ea26a11
Jim Nelson <jim@jimnelson.us>
2022-01-14 17:05:15 -0800

    sky tweaking

--- src/sky.lua
+++ src/sky.lua
@@ -5,7 +5,7 @@ local Game_component = require("generics.game-component")
 -- optimizations
 local graphics = love.graphics
 
-local sky_shader = graphics.newShader([[
+local shader_string = [[
     extern vec2 u_screen_size;
 
     vec4 effect( vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords ) {
@@ -14,11 +14,11 @@ local sky_shader = graphics.newShader([[
         dist = 1.0 - clamp(dist, 0.0, 0.4);
         return vec4(dist, dist, dist, 1.0) * color;
     }
-]])
+]]
 
 function Sky:new()
     local bg = Game_component:new()
-
+    bg.shader = love.graphics.newShader(shader_string)
     function bg:load()
     end
 
@@ -26,8 +26,8 @@ function Sky:new()
     end
 
     function bg:draw()
-        graphics.setShader(sky_shader)
-        sky_shader:send("u_screen_size", { graphics.getWidth(), graphics.getHeight()})
+        graphics.setShader(self.shader)
+        self.shader:send("u_screen_size", { graphics.getWidth(), graphics.getHeight()})
         graphics.setColor(0.529, 0.808, 0.922)
         graphics.rectangle("fill", 0, 0, graphics.getWidth(), graphics.getHeight())
         graphics.setShader()
